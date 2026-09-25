2f5c6e2d2ebc18dd2cee66ddb146d3f39d356d54
PKS-CUDJOE <ghosttech49@gmail.com>
2026-09-25 02:40:33 +0000

    A CONSOLE BASED C++ PROGRAM TO CHECK WHETHER A  NUMBER IS A BITWISE PALINDROME OR NOT

---
+++ bitwise_palindromes.cpp
@@ -0,0 +1,35 @@
+// A C++ PROGRAM TO CHECK IF AN INTEGER HAS A PALINDROME BIT REPRESENTATION
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+    unsigned short int val;
+
+    bool ispalindrome = false;
+
+    cout << "value = ";
+    cin >> val;
+
+    unsigned short int newval = 0;
+
+    for(int i = 0; i < 16; i++){
+        int bit = val & (1 << i);
+
+        .
+        if(bit)
+            newval |= (1 << (15-i));
+    }
+
+
+    ispalindrome = newval == val;
+
+    if(ispalindrome)
+        cout << val << " is a bitwise palindrome" << endl;
+    else
+        cout << val << " is not a bitwise palindrome" << endl;
+    return 0;
+}
